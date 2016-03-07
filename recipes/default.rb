a209e20475d094970196b8622d86efcde6781be6
Michael Wright <michael.wright@buildempire.co.uk>
2016-03-07 12:59:29 +0000

    Update paths

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-include_recipe "appbox::package_update"
-include_recipe "appbox::users"
-include_recipe "appbox::apps_dir"
-include_recipe "appbox::tools"
+include_recipe "cookbook_appbox::package_update"
+include_recipe "cookbook_appbox::users"
+include_recipe "cookbook_appbox::apps_dir"
+include_recipe "cookbook_appbox::tools"
